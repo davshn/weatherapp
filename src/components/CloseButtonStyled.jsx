b76226a0ff4e7cbe8ebb8975ac70f8bedae33609
Hernán David Figueroa <davshn@gmail.com>
2021-10-21 18:03:26 -0500

    Documented

--- src/components/CloseButtonStyled.jsx
+++ src/components/CloseButtonStyled.jsx
@@ -1,25 +1,20 @@
 import styled from "styled-components"
 //Estilo para el boton de cerrar
 export const CloseButton = styled.button`
-
-    //Tu codigo acá
-    //en Styled components no es necesario usar identificadores
-    background: none;       
-    color: white;
-    border: 2px solid white;
-    border-radius: 40%;
-    transition: all 1s ease-out;   //la animacion dura 1 segundo 
-    float: right;
-    margin-top: -30px;
-    margin-right: 10px;
-    
-        
-    &:hover {                       //Para declarar el over o cualquier seudo clase se usa el & como identificador
-      transform:  rotateZ(360deg);  //rotacion
-      color: #eb53ff;
-      background-color: #fff;
-      opacity: 0.5;
-      
+  background: none;       
+  color: white;
+  border: 2px solid white;
+  border-radius: 40%;
+  transition: all 1s ease-out;   //la animacion dura 1 segundo 
+  float: right;
+  margin-top: -30px;
+  margin-right: 10px;
+  
+  &:hover {                       //Para declarar el over o cualquier seudo clase se usa el & como identificador
+    transform:  rotateZ(360deg);  //rotacion
+    color: #eb53ff;
+    background-color: #fff;
+    opacity: 0.5;
     } 
     
 `;
