0a604e2fbd472ff16cdfb054f83e203570f4c1d0
Hernán David Figueroa <davshn@gmail.com>
2021-10-21 14:26:31 -0500

    Search bar fixed, documented

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,14 +3,13 @@ import { useState } from "react";
 //Estilo para los botones de la aplicacion
 const SubmitButton = styled.input`
 //Tu codigo aqui
-
-width: 90px;
-height: 30px;
-border-radius: 5px;
-color: #fff;
-background: transparent;
-border: #fff 2px solid;
-font-weight: bolder;
+  width: 90px;
+  height: 30px;
+  border-radius: 5px;
+  color: #fff;
+  background: transparent;
+  border: #fff 2px solid;
+  font-weight: bolder;
 
 &:hover{
   transform : scale(0.9);
@@ -22,28 +21,25 @@ font-weight: bolder;
 `
 const TextSearch = styled.input`
 //Tu codigo aqui
-margin: 5px;
-height: 26px;
-border-radius: 5px;
-border: #fff 1px solid;
-
-opacity: 0.6;
+  margin: 5px;
+  height: 26px;
+  border-radius: 5px;
+  border: #fff 1px solid;
+  opacity: 0.6;
 
 `
 
-
 export default function SearchBar({onSearch}) {
-  const [city,setCity]  = useState("")
+  const [city,setCity]  = useState("") //estado de cambio de ciudad escrita en el buscador
   
       return (
-        <form onSubmit={(e) => {
+        <form onSubmit={(e) => {    //Cuando se envía el valor busca en la API
           e.preventDefault();
           onSearch(city);
         }}>
           <TextSearch
-            onChange={(e) => {
+            onChange={(e) => {      //Cuando cambia la ciudad digitada guarda el valor en el estado
               e.preventDefault();
-              console.log(e.target.value);
               setCity(e.target.value);
             }}
             type="text"
