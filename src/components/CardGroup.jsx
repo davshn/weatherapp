3d2cbe56e0f2a4b294f85effe4c5b939fd38d2e6
Hernán David Figueroa <davshn@gmail.com>
2021-10-21 18:05:14 -0500

    Documented

--- src/components/CardGroup.jsx
+++ src/components/CardGroup.jsx
@@ -3,15 +3,15 @@ import styled from "styled-components"
 import propTypes from "prop-types";
 
 const CardContainer = styled.section`               //Estilo del contenedor de tarjetas
-//Tu codigo acá
+
 
 `;
 
 export default function CardGroup({cities,onClose}) {
   
   if(cities){
-  return (
-  <CardContainer>    
+    return (
+    <CardContainer>    
     {
       cities.map((city)=> <WeatherCard 
         max={city.max} 
@@ -23,8 +23,8 @@ export default function CardGroup({cities,onClose}) {
         id={city.id}/>               
       )
     }{/*para cada elemento del array cities, crea por funcion flecha una tarjeta de clima para cada elemento*/}
-  </CardContainer>
-  )
+    </CardContainer>
+    )
   }
   else {return <span>Sin ciudades</span>}
 };
