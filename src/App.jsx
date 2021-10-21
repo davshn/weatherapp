b2c3cfc656acb64444ace0c077d5db9b60199c2c
Hernán David Figueroa <davshn@gmail.com>
2021-10-21 02:04:00 -0500

    Main app styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import CardGroup from './components/CardGroup';
 import NavigationBar from './components/NavigationBar';
 import data from './data.js';
+import styled from 'styled-components';
+
+const MainApp = styled.main`
+  //Tu codigo aqui
+
+
+`
+
+
+
 
 function App() {
   return (
     
-    <main>    {/*Crea el arreglo de ciudades y les pasa parametros*/}
+    <MainApp>    {/*Crea el arreglo de ciudades y les pasa parametros*/}
       <NavigationBar/>
       <CardGroup                               
         cities={data}
       />
-    </main>
+    </MainApp>
         
   );
 }
